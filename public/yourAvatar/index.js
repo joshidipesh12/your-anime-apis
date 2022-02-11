9e2a3cb4413bc25deaec246aa8c626061fba9012
Dipesh Joshi <joshidipesh12@gmail.com>
2022-02-11 21:04:29 +0530

    background-blur-img trial

--- public/yourAvatar/index.js
+++ public/yourAvatar/index.js
@@ -2,12 +2,15 @@ var urlCreator = window.URL || window.webkitURL;
 
 let uploaded = document.getElementById('upload-img');
 let input = document.getElementsByClassName('upload-input')[0];
-let input_img = document.getElementsByClassName('input-img')[0];
 let original_div = document.getElementsByClassName('original')[0];
 let avatar_div = document.getElementsByClassName('avatar_container')[0];
 let avatar_loader = document.getElementsByClassName('loader')[0];
 let imgForm = document.getElementsByClassName('img-form')[0];
-let avatarImg = document.getElementsByClassName('avatar-img')[0];
+
+let input_img = document.getElementsByClassName('input-img')[0];
+let input_img_bg = document.getElementsByClassName('input-img blur')[0];
+let avatar_img = document.getElementsByClassName('avatar-img')[0];
+let avatar_img_bg = document.getElementsByClassName('avatar-img blur')[0];
 
 uploaded.addEventListener('change', () => {
   const file_url = urlCreator.createObjectURL(uploaded.files[0]);
@@ -15,20 +18,22 @@ uploaded.addEventListener('change', () => {
   avatar_div.style.display = 'flex';
   input.style.display = 'flex';
   input_img.src = file_url;
+  input_img_bg.src = file_url;
 
   const imgFormData = new FormData(imgForm);
   axios
     .post('/v1/yourAvatar', imgFormData)
     .then(resp => {
       const resImage = `data:image/png;base64,${resp.data}`;
-      avatarImg.src = resImage;
+      avatar_img.src = resImage;
+      avatar_img_bg.src = resImage;
     })
     .catch(err => console.log(err));
 });
 
-avatarImg.addEventListener('load', e => {
+avatar_img.addEventListener('load', e => {
   document.getElementsByClassName('loader')[0].style.display = 'none';
-  avatarImg.style.position = 'block';
+  avatar_img.style.position = 'block';
   anime({
     targets: '.avatar-img',
     opacity: 1,
