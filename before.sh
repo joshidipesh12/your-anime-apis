d63b3df6447fc3e27907f8d86d537e0a07970e33
Dipesh Joshi <joshidipesh12@gmail.com>
2022-03-08 22:30:24 +0530

    renamed python-dev

--- before.sh
+++ before.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 echo "Installing python..."
-apt-get install python3-pip python-dev
+apt-get install python3-pip python
 
 echo "Installing Python Dependencies"
 pip install numpy
