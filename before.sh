8c2747d5d1551a449467e556dc7adc51dab1e7e0
Dipesh Joshi <joshidipesh12@gmail.com>
2022-03-08 22:21:19 +0530

    removed sudo

--- before.sh
+++ before.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 echo "Installing python..."
-sudo apt-get install python3-pip python-dev
+apt-get install python3-pip python-dev
 
 echo "Installing Python Dependencies"
 pip install numpy
