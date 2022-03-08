5bfd1ef6262982eac6c910684c673b2bc756db15
Dipesh Joshi <joshidipesh12@gmail.com>
2022-03-08 22:32:17 +0530

    python version

--- before.sh
+++ before.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 echo "Installing python..."
-apt-get install python3-pip python
+python --version
+
+# apt-get install python3-pip python
 
 echo "Installing Python Dependencies"
 pip install numpy
