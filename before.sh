21389778658170ec939a8f20ce1516fde541a08d
Dipesh Joshi <joshidipesh12@gmail.com>
2022-03-09 09:50:00 +0530

    trial 2 with reorder

--- before.sh
+++ before.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env sh
 
-echo "Installing python..."
 python --version
 # add-apt-repository universe
 # apt-get update
 # apt-get install python3-pip
 
-echo "Installing Python Dependencies"
+echo "Installing Numpy Dipesh JI"
 pip3 install numpy
 echo "Install Complete!"
