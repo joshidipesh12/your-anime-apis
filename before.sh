b2d04c28b579bca240990667667e17ec8e120d39
Dipesh Joshi <joshidipesh12@gmail.com>
2022-03-09 10:37:18 +0530

    replaced script with requirements.txt

--- before.sh
+++ before.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 python --version
-# add-apt-repository universe
-# apt-get update
-# apt-get install python3-pip
+add-apt-repository universe
+apt-get update
+apt-get install python3-pip
 
 echo "Installing Numpy Dipesh JI"
 pip3 install numpy
