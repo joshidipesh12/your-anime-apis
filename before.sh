cd23afd8050c04595b0da56ae748584b0f3afe76
Dipesh Joshi <joshidipesh12@gmail.com>
2022-03-09 09:47:58 +0530

    trial with new buildpack

--- before.sh
+++ before.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 echo "Installing python..."
-# python --version
-add-apt-repository universe
-apt-get update
-apt-get install python3-pip
+python --version
+# add-apt-repository universe
+# apt-get update
+# apt-get install python3-pip
 
 echo "Installing Python Dependencies"
 pip3 install numpy
