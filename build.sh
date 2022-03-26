68678a3172fea9b83ed77de0b613f2da620c6144
Dipesh Joshi <joshidipesh12@gmail.com>
2022-03-26 17:43:04 +0530

    added model download script

---
+++ build.sh
@@ -0,0 +1,8 @@
+FILE=./app/v1/your/predictor/model_ep_1.h5
+if test -f "$FILE"; then
+    echo "Model Already Exists."
+else 
+    echo "Downloading Model..."
+    wget 'https://drive.google.com/uc?id=1C8EPUUO1RiD8KaRK_s1JJinsGPVidUK5&alt=media' -O "$FILE"
+    echo "Download Complete !!"
+fi
